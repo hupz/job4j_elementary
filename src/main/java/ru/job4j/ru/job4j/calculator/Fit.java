6a03af67a489cd1dfbb55d384e25f64b541e41a7
Vedmak <icefortess@gmail.com>
2022-02-23 06:58:02 +0300

    6. Идеальный вес [#41584]

---
+++ src/main/java/ru/job4j/ru/job4j/calculator/Fit.java
@@ -0,0 +1,23 @@
+package ru.job4j.ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        return height * 1.15;
+    }
+
+    public static double womanWeight(short height) {
+        return height * 1.15;
+    }
+
+    public static void main(String[] args) {
+        short heightMan = 187;
+        short heightWoman = 167;
+        double man = Fit.manWeight(heightMan);
+        double woman = Fit.womanWeight(heightWoman);
+        System.out.println("Man 187 is " + man);
+        System.out.println("Woman 167 is " + woman);
+    }
+
+}
+
