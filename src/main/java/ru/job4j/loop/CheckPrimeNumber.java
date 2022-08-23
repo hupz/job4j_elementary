a46127beeaab76ae56038724d9c70d133f78ac26
Vedmak <icefortess@gmail.com>
2022-08-23 08:57:39 +0300

    Простое число

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,12 +2,11 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = false;
-        for (int num = 2; num <= number - 1; num++) {
-            if ((number % num) == 0) {
-                    break;
-            } else {
-                prime = true;
+        boolean prime = number > 1;
+        for (int num = 2; num < number; num++) {
+            if (number % num == 0) {
+                prime = false;
+                break;
             }
         }
         return prime;
