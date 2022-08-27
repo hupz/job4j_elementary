766b076dfb38ee62f175eedc1867000288927c3e
Vedmak <icefortess@gmail.com>
2022-08-27 09:06:45 +0300

    Классический поиск перебором.

---
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FindLoopTest {
+    @Test
+    public void whenArrayHas5Then0() {
+        int[] data = new int[] {5, 10, 3};
+        int el = 5;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenArrayHasNot10ThenMinus1() {
+        int[] data = new int[] {8, 7, 15, 59};
+        int el = 10;
+        int result = FindLoop.indexOf(data, el);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenArrayHas8Then4() {
+        int[] data = new int[] {6, 7, 15, 59, 8};
+        int el = 8;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+}
